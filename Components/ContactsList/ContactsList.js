85a4c421a9e2e780773b4081e2f32219648636d6
sergaik-web <seroga8863@gmail.com>
2020-10-15 01:24:03 +0300

    feat: get contacts

--- Components/ContactsList/ContactsList.js
+++ Components/ContactsList/ContactsList.js
@@ -3,27 +3,26 @@ import {Container, Text} from "native-base";
 import * as Contacts from 'expo-contacts';
 
 const ContactsList = ()=>{
-  // const [cont, setContacts] = useState([]);
-  // useEffect(() => {
-  //   (async () => {
-  //     const { status } = await Contacts.requestPermissionsAsync();
-  //     if (status === 'granted') {
-  //       const { data } = await Contacts.getContactsAsync({
-  //         fields: [Contacts.Fields.Emails],
-  //       });
-  //
-  //       if (data.length > 0) {
-  //         const contact = data[0];
-  //         console.log(contact);
-  //       }
-  //     }
-  //   })();
-  // }, []);
+  const [cont, setContacts] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync();
+
+        if (data.length > 0) {
+          const contact = data[356];
+          setContacts(contact);
+          console.log(contact);
+        }
+      }
+    })();
+  }, []);
 
   return (
     <Container>
       <Text>
-        'loading'
+        {cont ? cont.id : 'loading'}
       </Text>
     </Container>
   )
